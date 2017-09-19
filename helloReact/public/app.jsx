18e25c695030000ec644a7eaea7f255a4d037d7a
Carlos A A Barreto <araujobarret@gmail.com>
2017-09-19 12:03:09 -0300

    Events and callbacks

--- helloReact/public/app.jsx
+++ helloReact/public/app.jsx
@@ -5,12 +5,24 @@ let Greeter = React.createClass({
       message: 'This is from the component.'
     };
   },
+  onButtonClick: function(e) {
+    e.preventDefault();
+
+    let name = this.refs.name.value;
+    console.log(this.refs.name);
+    alert(name);
+  },
   render: function (){
     let name =  this.props.name;
     return (
       <div>
         <h1>Hello {name}!</h1>
         <p>{this.props.message}</p>
+
+        <form onSubmit={this.onButtonClick}>
+          <input type="text" ref="name"/>
+          <button>Set name</button>
+        </form>
       </div>
     );
   }
